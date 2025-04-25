85053f818d4e64b778fbe80b2c204b1f238fefb7
Andre Kaltenbach <andre.kaltenbach@posteo.de>
2025-04-25 15:24:05 +0200

    solve iteration 1 - lift state up

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import "./App.css";
-import Counter from "./components/Counter";
-import ExponentTwo from "./components/ExponentTwo";
-import ExponentThree from "./components/ExponentThree";
-import ExponentFour from "./components/ExponentFour";
-import ExponentFive from "./components/ExponentFive";
-import ExponentSix from "./components/ExponentSix";
+import './App.css';
+import { useState } from 'react';
 
+import Counter from './components/Counter';
+import ExponentTwo from './components/ExponentTwo';
+import ExponentThree from './components/ExponentThree';
+import ExponentFour from './components/ExponentFour';
+import ExponentFive from './components/ExponentFive';
+import ExponentSix from './components/ExponentSix';
 
-function App () {
+function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <h2><em>Counter</em></h2>
-  
-      <Counter/>
+      <h2>
+        <em>Counter</em>
+      </h2>
+
+      <Counter count={count} setCount={setCount} />
 
       <br />
-      <h2><em>Exponents</em></h2>
+      <h2>
+        <em>Exponents</em>
+      </h2>
 
       <div className="container">
         <ExponentTwo />
