4d11c31fcf2d100a04042569875a0a2e75ffc2ab
Andre Kaltenbach <andre.kaltenbach@posteo.de>
2025-04-25 15:51:44 +0200

    solve iteration 3 - move state logic up

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,11 @@
 const Counter = (props) => {
-  const decrement = () => props.setCount((prevCount) => prevCount - 1);
-  const increment = () => props.setCount((prevCount) => prevCount + 1);
-
   return (
     <div className="counter-container">
       <p className="counter-value">{props.count}</p>
-      <button className="counter-button" onClick={decrement}>
+      <button className="counter-button" onClick={props.decrement}>
         -
       </button>
-      <button className="counter-button" onClick={increment}>
+      <button className="counter-button" onClick={props.increment}>
         +
       </button>
     </div>
